bad63377b3e69b8e9e5968b8123f1e0582d31b4c
johndward01 <johndward01@yahoo.com>
2020-07-28 20:58:24 -0500

    initial commit

---
+++ ASP.NET_Teacher_Demo_ConsoleUI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASP.NET_Teacher_Demo_ConsoleUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
